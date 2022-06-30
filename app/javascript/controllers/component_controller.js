32a7b8c3865ce69ac273a9315b4f65cb82808874
rodrigogaldino553 <galdinorodrigo553@gmail.com>
2022-06-30 15:20:02 -0300

    add car edit inline

--- app/javascript/controllers/component_controller.js
+++ app/javascript/controllers/component_controller.js
@@ -1,13 +1,22 @@
 import * as Vue from "vue"
+import { FetchRequest } from "@rails/request.js"
+
 const point = "#point"
 const element = document.querySelector(point)
 if (element !== null) {
   const app = Vue.createApp({
     data() {
-      return { count: 1, name: '' }
+      return { count: 1, name: '', car: {name: '', brand: '', price: 0} }
     },
     methods: {
+      async saveCar(){
+        const request = new FetchRequest('post', '/cars',
+        {
+          body: JSON.stringify({ car: this.car })
+        })
 
+        const response = await request.perform()
+      }
     },
     created() {
       console.log("count is: "+ this.count) // => "count is: 1"
--- config/importmap.rb
+++ config/importmap.rb
@@ -7,3 +7,4 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin "vue", to: "https://ga.jspm.io/npm:vue@3.2.26/dist/vue.esm-browser.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "application", preload: true
+pin "@rails/request.js", to: "https://ga.jspm.io/npm:@rails/request.js@0.0.6/src/index.js"
